fb616ad4d4e2b412ec46c9dc120af299143d3811
Akanksha <83341547+Akanksha108@users.noreply.github.com>
2021-07-26 20:16:40 +0530

    added function to calculate hypotenuse

---
+++ hypotenuse.js
@@ -0,0 +1,25 @@
+const angle_Input1 = document.getElementById("input_1");
+
+const angle_Input2 = document.getElementById("input_2");
+
+let submitHypo = document.getElementById("submit_btn");
+
+let output = document.getElementById("output");
+
+function calculateHypotenuse(event){
+
+    event.preventDefault();
+
+    const input1 = parseInt(angle_Input1.value);
+
+    const input2 = parseInt(angle_Input2.value);
+
+    let hypotenuse = Math.sqrt((input1 * input1) + (input2 * input2)); 
+
+    // console.log(hypotenuse);
+
+    output.innerHTML = "c = " + hypotenuse;
+    
+}
+
+submitHypo.addEventListener("click", calculateHypotenuse);
