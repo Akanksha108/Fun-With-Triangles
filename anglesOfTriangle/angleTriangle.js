1e77f85560ec9c197c69786f31ff98e1a20aefac
Akanksha <83341547+Akanksha108@users.noreply.github.com>
2021-08-15 16:27:55 +0530

    updated input validation and added error message

--- anglesOfTriangle/angleTriangle.js
+++ anglesOfTriangle/angleTriangle.js
@@ -8,6 +8,8 @@ const submitBtn = document.querySelector(".submit_btn");
 
 let output = document.querySelector(".output");
 
+let error = document.querySelector(".error");
+
 
 function angleTriangle(event){
 
@@ -19,19 +21,22 @@ function angleTriangle(event){
 
    const input3 = parseInt(angleInput3.value);
 
-   if ( (input1 < 180) && (input2 < 180) && (input3 < 180)){
-    let sum = input1 + input2 + input3;
-
-        if(sum == 180){
-            output.innerHTML = "YuHu!!! These angles can make a triangle";
-        }else{
-            output.innerHTML = "These angles cannot make a triangle";
-        }
+   if(input1 && input2 && input3){
+    if ( (input1 > 0) && (input1 < 180) && (input2 > 0) && (input2 < 180) && (input3 > 0) && (input3 < 180)){
+        let sum = input1 + input2 + input3;
+    
+            if(sum == 180){
+                output.innerHTML = "YuHu!!! These angles can make a triangle";
+            }else{
+                output.innerHTML = "These angles cannot make a triangle";
+            }
+       }
+       else{
+           error.innerHTML = "Angle values cannot be greater than 180";
+       }
+   }else{
+       error.innerHTML = "Please fill all the required fields";
    }
-   else{
-       output.innerHTML = "Please enter valid angle values";
-   }
-   
 
 }
 
